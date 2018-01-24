87032a0f660aa1e70dbe74e668a51ccc54723579
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-01-24 21:54:35 +0100

    get message details from DOM

--- src/trello-integration.ts
+++ src/trello-integration.ts
@@ -22,5 +22,44 @@ export function createTrelloCard(title?: string, description?: string, attachmen
 
 export function trelloClickHandler(event: Event) {
 	console.log('LOG: onclick', event);
-	createTrelloCard();
+	const { subject, body } = getMessageDetails(event.target);
+	const searchUrl = buildInboxSearchUrl({ subject, body });
+	console.log('LOG: url', searchUrl);
+	chrome.runtime.sendMessage({ openPopup: searchUrl });
+
+	// createTrelloCard(subject, body);
+}
+
+interface MessageDetails {
+	subject?: string;
+	body?: string;
+}
+
+function getMessageDetails(toolbarTarget: EventTarget): MessageDetails {
+	const toolbarElement = toolbarTarget as HTMLElement;
+
+	let parentElement: Element;
+	let listParent = toolbarElement.closest('.l9');
+	let messageParent = toolbarElement.closest('.aY.ac.X.s2');
+	if (listParent) {
+		// the button is pressed in the list view
+		parentElement = listParent;
+	} else if (messageParent) {
+		// the button is pressed in the message view
+		parentElement = messageParent;
+	} else {
+		throw new Error(
+			'The Trello button was not clicked in a list or message view. Where was it clicked?',
+		);
+	}
+	const subject = parentElement.querySelector('.bg > span')!.textContent!;
+	const body = parentElement.querySelector('.g6 > span')!.textContent!;
+	return { subject, body };
+}
+
+function buildInboxSearchUrl({ subject, body }: MessageDetails) {
+	const baseUrl = 'https://inbox.google.com/u/0/search/';
+	const searchPart = encodeURIComponent(`subject:"${subject}" "${body}"`);
+	// add rfc822msgid
+	return baseUrl + searchPart;
 }
